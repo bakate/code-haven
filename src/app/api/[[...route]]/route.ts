a7c52a0add514da20ea7e78bf46f9597ef99daf2
Bakate <b.ba@startupflow.io>
2024-06-23 15:29:42 +0200

    feat(auth-js): implement auth.js middleware to handle authentication and authorization

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -1,11 +1,15 @@
-import { Hono } from "hono";
+import { authHandler, initAuthConfig, type AuthConfig } from "@hono/auth-js";
+import { Context, Hono } from "hono";
 import { handle } from "hono/vercel";
 
+import Google from "@auth/core/providers/google";
 import user from "./user";
 
 export const runtime = "edge";
 
 const app = new Hono().basePath("/api");
+app.use("*", initAuthConfig(getAuthConfig));
+app.use("/auth/*", authHandler());
 
 const routes = app.route("/users", user);
 
@@ -13,6 +17,18 @@ export const GET = handle(app);
 export const POST = handle(app);
 export const PATCH = handle(app);
 export const DELETE = handle(app);
+
+function getAuthConfig(c: Context): AuthConfig {
+  return {
+    secret: process.env.AUTH_SECRET,
+    providers: [
+      Google({
+        clientId: process.env.AUTH_GOOGLE_ID,
+        clientSecret: process.env.AUTH_GOOGLE_SECRET,
+      }),
+    ],
+  };
+}
 
 export type AppType = typeof routes;
 // export default app; // to challenge since I have this warning:
