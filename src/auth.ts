261a9f237ea08eac9c4b22c81b49b59cd86d7077
Bakate <b.ba@startupflow.io>
2024-06-23 23:57:04 +0200

    feat(auth): add resend provider to auth configuration to implement magic link

--- src/auth.ts
+++ src/auth.ts
@@ -3,15 +3,16 @@ import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import { db } from "@/db/drizzle";
 import Google from "@auth/core/providers/google";
 import { AuthConfig } from "@hono/auth-js";
+import Resend from "next-auth/providers/resend";
 
 export function getAuthConfig(): AuthConfig {
   return {
     adapter: DrizzleAdapter(db),
     secret: process.env.AUTH_SECRET,
     providers: [
-      Google({
-        clientId: process.env.AUTH_GOOGLE_ID,
-        clientSecret: process.env.AUTH_GOOGLE_SECRET,
+      Google,
+      Resend({
+        from: process.env.RESEND_FROM_EMAIL,
       }),
     ],
   };
