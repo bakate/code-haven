c36d077439820b9344cf100d8fc0409eb8b70a8a
Bakate <b.ba@startupflow.io>
2024-07-22 21:34:59 +0200

    chore: add react-query for client data fetching

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,8 +4,8 @@ import type { Metadata } from "next";
 import { SessionProvider } from "next-auth/react";
 import { NextIntlClientProvider } from "next-intl";
 import { getLocale, getMessages } from "next-intl/server";
+import { Providers } from "../providers/providers";
 import "./globals.css";
-import { Providers } from "./providers";
 
 export const metadata: Metadata = {
   title: "Code Haven",
--- src/providers/providers.tsx
+++ src/providers/providers.tsx
@@ -3,14 +3,17 @@
 import { NextUIProvider } from "@nextui-org/react";
 import { useRouter } from "next/navigation";
 import { Toaster } from "sonner";
+import { QueryProvider } from "./query-provider";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   const router = useRouter();
 
   return (
-    <NextUIProvider navigate={router.push}>
-      <Toaster position="bottom-right" closeButton richColors />
-      {children}
-    </NextUIProvider>
+    <QueryProvider>
+      <NextUIProvider navigate={router.push}>
+        <Toaster position="bottom-right" closeButton richColors />
+        {children}
+      </NextUIProvider>
+    </QueryProvider>
   );
 }
