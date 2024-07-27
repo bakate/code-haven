ee7fec7e26face3682c11d9c18cc101922f65ca6
Bakate <b.ba@startupflow.io>
2024-07-28 00:23:47 +0200

    feat(customHook): add useClientCheck hook

---
+++ src/features/auth/hooks/use-client-check.tsx
@@ -0,0 +1,14 @@
+"use client";
+import { useEffect, useState } from "react";
+
+const useClientCheck = () => {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  return isClient;
+};
+
+export default useClientCheck;
