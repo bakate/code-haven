8e0986b68e4f61becd0d7e12bedf7951a1f85f9f
Bakate <b.ba@startupflow.io>
2024-08-15 19:53:33 +0200

    fix(auth): improve the style to push the footer down correctly

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -6,7 +6,7 @@ export default function AuthScreenLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="grid min-h-[100dvh] grid-rows-[1fr_auto]">
       <div className="flex-1 flex flex-col justify-center items-center">
         {children}
       </div>
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,7 +5,7 @@ import { LocalSwitcherSelect } from "./local-switcher-select";
 export const Footer = () => {
   const t = useTranslations("footer");
   return (
-    <div className="flex flex-col items-center w-full  h-16 bg-white border-t border-gray-200 shadow-md">
+    <div className="flex flex-col items-center w-full h-16 bg-white border-t border-gray-200 shadow-md dark:bg-slate-900 dark:border-t-slate-700">
       <div className=" flex items-center w-full h-full">
         <p className="text-gray-500 text-center flex-grow md:pl-44">
           {t("copyright")}
