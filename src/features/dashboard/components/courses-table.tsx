8215e4bb4f289374d9ee11f841f511b9977161df
Bakate <b.ba@startupflow.io>
2024-08-16 22:42:46 +0200

    feat(teacherCourses): display a CourseTableSkeleton when fetching data

--- src/features/dashboard/components/courses-table.tsx
+++ src/features/dashboard/components/courses-table.tsx
@@ -4,43 +4,63 @@ import { useLocale, useTranslations } from "next-intl";
 import { Key, useCallback, useMemo, useReducer } from "react";
 import { useGetCoursesByTeacher } from "../data/use-get-courses-by-teacher";
 
-import { Table, TableBody, TableCell, TableColumn, TableHeader, TableRow } from "@nextui-org/react";
+import {
+  Skeleton,
+  Table,
+  TableBody,
+  TableCell,
+  TableColumn,
+  TableHeader,
+  TableRow,
+} from "@nextui-org/react";
 import { useGetCategories } from "../data/use-get-categories";
 import { StateType } from "../types";
-import { coursesTableReducer, initialCourseTableState } from "./courses-table/reducer";
+import {
+  coursesTableReducer,
+  initialCourseTableState,
+} from "./courses-table/reducer";
 import { TableBottomContent } from "./courses-table/table-bottom-content";
 import { TableRowContent } from "./courses-table/table-row-content";
 import { TableTopContent } from "./courses-table/table-top-content";
-
-
+import { CourseTableSkeleton } from "./courses-table/course-table-skeleton";
 
 export const CoursesTable = () => {
   const t = useTranslations("createOrEditCourseForm");
   const locale = useLocale();
-  const { data: allCategories, isLoading: areCategoriesLoading } = useGetCategories();
-  const { data: allCourses, isError, isLoading, isFetched } = useGetCoursesByTeacher();
+  const { data: allCategories, isLoading: areCategoriesLoading } =
+    useGetCategories();
+  const {
+    data: allCourses,
+    isError,
+    isLoading,
+    isFetched,
+  } = useGetCoursesByTeacher();
 
+  const [state, dispatch] = useReducer(
+    coursesTableReducer,
+    initialCourseTableState
+  );
 
-  const [state, dispatch] = useReducer(coursesTableReducer, initialCourseTableState)
-
-  const statusOptions = useMemo(() => [
-    { name: t('published'), uid: "published" },
-    { name: t('unpublished'), uid: "unpublished" },
-  ], [t])
-
+  const statusOptions = useMemo(
+    () => [
+      { name: t("published"), uid: "published" },
+      { name: t("unpublished"), uid: "unpublished" },
+    ],
+    [t]
+  );
 
   const hasSearchFilter = Boolean(state.filterValue);
 
   const headerColumns = useMemo(() => {
     const columns = [
       { name: "ID", uid: "id", sortable: true },
-      { name: t('titleLabel'), uid: "titles", sortable: true },
-      { name: t('priceLabel'), uid: "price", sortable: true },
-      { name: t('published'), uid: "isPublished", sortable: true },
+      { name: t("titleLabel"), uid: "titles", sortable: true },
+      { name: t("priceLabel"), uid: "price", sortable: true },
+      { name: t("published"), uid: "isPublished", sortable: true },
       { name: t("category"), uid: "categoryId", sortable: true },
       { name: t("actions"), uid: "actions", sortable: false },
     ];
-    if (state.visibleColumns.has('all')) return columns;
+    if (state.visibleColumns.has("all")) return columns;
     return columns.filter((column) =>
       Array.from(state.visibleColumns).includes(column.uid)
     );
@@ -50,7 +70,10 @@ export const CoursesTable = () => {
     let filteredCourses = allCourses ? [...allCourses] : [];
     if (hasSearchFilter) {
       filteredCourses = filteredCourses.filter((course) =>
-        course.titles.find(t => t.lang === locale)?.title?.toLowerCase().includes(state.filterValue.toLowerCase())
+        course.titles
+          .find((t) => t.lang === locale)
+          ?.title?.toLowerCase()
+          .includes(state.filterValue.toLowerCase())
       );
     }
     if (
@@ -58,23 +81,29 @@ export const CoursesTable = () => {
       Array.from(state.statusFilter).length !== statusOptions.length
     ) {
       filteredCourses = filteredCourses.filter((course) =>
-        Array.from(state.statusFilter).includes(course.isPublished ? "published" : "unpublished")
+        Array.from(state.statusFilter).includes(
+          course.isPublished ? "published" : "unpublished"
+        )
       );
     }
 
     return filteredCourses;
-  }, [allCourses, locale, hasSearchFilter, state.statusFilter, state.filterValue, statusOptions.length]);
+  }, [
+    allCourses,
+    locale,
+    hasSearchFilter,
+    state.statusFilter,
+    state.filterValue,
+    statusOptions.length,
+  ]);
 
-
-  const pages = Math.ceil(filteredItems.length / state.rowsPerPage)
-
+  const pages = Math.ceil(filteredItems.length / state.rowsPerPage);
 
   const items = useMemo(() => {
     const start = (state.page - 1) * state.rowsPerPage;
     const end = start + state.rowsPerPage;
     return filteredItems.slice(start, end);
   }, [state.page, filteredItems, state.rowsPerPage]);
-
 
   const sortedItems = useMemo(() => {
     return [...items].sort((a, b) => {
@@ -85,43 +114,87 @@ export const CoursesTable = () => {
     });
   }, [items, state.sortDescriptor]);
 
-
-
-  const handleSelectionChange = useCallback((keys: 'all' | Set<Key>) => {
-    if (keys === 'all') {
-      dispatch({ type: 'SET_SELECTED_KEYS', payload: new Set<string>(items.map(item => item.id.toString())) });
-    } else {
-      dispatch({ type: 'SET_SELECTED_KEYS', payload: new Set<string>(Array.from(keys).map(key => key.toString())) });
-    }
-  }, [items]);
-
+  const handleSelectionChange = useCallback(
+    (keys: "all" | Set<Key>) => {
+      if (keys === "all") {
+        dispatch({
+          type: "SET_SELECTED_KEYS",
+          payload: new Set<string>(items.map((item) => item.id.toString())),
+        });
+      } else {
+        dispatch({
+          type: "SET_SELECTED_KEYS",
+          payload: new Set<string>(
+            Array.from(keys).map((key) => key.toString())
+          ),
+        });
+      }
+    },
+    [items]
+  );
 
   if (isLoading || !isFetched || areCategoriesLoading) {
-    return <div>Loading...</div>;
+    return <CourseTableSkeleton headerColumns={headerColumns} />;
   }
   if (isError) {
+    // TODO handle error gracefully
     return <div>Error</div>;
   }
   if (!allCourses) {
-    return <div>No data</div>;
+    return (
+      <Table aria-label="Courses list for a specific teacher" isHeaderSticky>
+        <TableHeader columns={headerColumns}>
+          {(column) => (
+            <TableColumn
+              className="capitalize"
+              key={column.uid}
+              hideHeader={column.uid === "actions"}
+              allowsSorting={column.sortable}
+            >
+              {column.name}
+            </TableColumn>
+          )}
+        </TableHeader>
+        <TableBody emptyContent={t("youHaveNotCreatedAnyCourse")}>
+          {[]}
+        </TableBody>
+      </Table>
+    );
   }
 
   return (
     <Table
       aria-label="Courses list for a specific teacher"
       isHeaderSticky
-      bottomContent={<TableBottomContent dispatch={dispatch} pages={pages} state={state} totalItems={filteredItems.length} />}
+      bottomContent={
+        <TableBottomContent
+          dispatch={dispatch}
+          pages={pages}
+          state={state}
+          totalItems={filteredItems.length}
+        />
+      }
       bottomContentPlacement="outside"
       classNames={{
         wrapper: "max-h-[382px]",
       }}
       selectedKeys={state.selectedKeys}
       selectionMode="multiple"
-      topContent={<TableTopContent state={state} dispatch={dispatch} statusOptions={statusOptions} headerColumns={headerColumns} />}
+      topContent={
+        <TableTopContent
+          state={state}
+          dispatch={dispatch}
+          statusOptions={statusOptions}
+          headerColumns={headerColumns}
+        />
+      }
       topContentPlacement="outside"
       sortDescriptor={state.sortDescriptor}
       onSortChange={(sortDescriptor) => {
-        dispatch({ type: 'SET_SORT_DESCRIPTOR', payload: sortDescriptor as StateType['sortDescriptor'] });
+        dispatch({
+          type: "SET_SORT_DESCRIPTOR",
+          payload: sortDescriptor as StateType["sortDescriptor"],
+        });
       }}
       onSelectionChange={handleSelectionChange}
     >
@@ -136,17 +209,24 @@ export const CoursesTable = () => {
             {column.name}
           </TableColumn>
         )}
-
       </TableHeader>
-      <TableBody emptyContent={t('noCoursesFound')} items={sortedItems}>
+      <TableBody emptyContent={t("noCoursesFound")} items={sortedItems}>
         {(item) => (
           <TableRow key={item.id}>
             {(columnKey) => (
-              <TableCell>{<TableRowContent course={item} allCategories={allCategories} columnKey={columnKey} />}</TableCell>
+              <TableCell>
+                {
+                  <TableRowContent
+                    course={item}
+                    allCategories={allCategories}
+                    columnKey={columnKey}
+                  />
+                }
+              </TableCell>
             )}
           </TableRow>
         )}
       </TableBody>
     </Table>
   );
-}
+};
---
+++ src/features/dashboard/components/courses-table/course-table-skeleton.tsx
@@ -0,0 +1,62 @@
+import {
+  Skeleton,
+  Table,
+  TableBody,
+  TableCell,
+  TableColumn,
+  TableHeader,
+  TableRow,
+} from "@nextui-org/react";
+
+type SkeletonProps = {
+  headerColumns: {
+    name: string;
+    uid: string;
+    sortable: boolean;
+  }[];
+};
+export const CourseTableSkeleton = ({ headerColumns }: SkeletonProps) => {
+  const RowSkeleton = () => <Skeleton className="h-10 w-full" />;
+  return (
+    <Table aria-label="Courses list for a specific teacher" isHeaderSticky>
+      <TableHeader columns={headerColumns}>
+        {(column) => (
+          <TableColumn
+            className="capitalize"
+            key={column.uid}
+            hideHeader={column.uid === "actions"}
+            allowsSorting={column.sortable}
+          >
+            {column.name}
+          </TableColumn>
+        )}
+      </TableHeader>
+      <TableBody>
+        {[...Array(5)].map((_, index) => (
+          <TableRow key={index}>
+            <TableCell className="flex gap-2">
+              <Skeleton className="h-10 w-10 rounded-full" />
+              <RowSkeleton />
+            </TableCell>
+            <TableCell>
+              <RowSkeleton />
+            </TableCell>
+            <TableCell>
+              <RowSkeleton />
+            </TableCell>
+            <TableCell>
+              <RowSkeleton />
+            </TableCell>
+            <TableCell>
+              <div className="flex justify-center items-center flex-col gap-1">
+                <Skeleton className="size-1 rounded-full animate-pulse" />
+                <Skeleton className="size-1 rounded-full animate-pulse" />
+                <Skeleton className="size-1 rounded-full animate-pulse" />
+              </div>
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
+};
