afa2ee44d85f5afc4cdb08083f54e575b466e6bd
Bakate <b.ba@startupflow.io>
2024-08-18 07:35:40 +0200

    feat(teacherCourses): optimize TableTopContent component for better performance

--- src/features/dashboard/components/courses-table/table-top-content.tsx
+++ src/features/dashboard/components/courses-table/table-top-content.tsx
@@ -1,55 +1,72 @@
-import { Button, Dropdown, DropdownItem, DropdownMenu, DropdownTrigger, Input, Link } from "@nextui-org/react";
-import { Dispatch } from "react";
+import {
+  Button,
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownTrigger,
+  Input,
+  Link,
+} from "@nextui-org/react";
+import { Dispatch, useState } from "react";
 import { LuChevronDown, LuPlus, LuSearch } from "react-icons/lu";
 import { CourseTableAction } from "./reducer";
 
 import { useTranslations } from "next-intl";
-import { StateType } from "../../types";
 import { useMedia } from "react-use";
+import { StateType } from "../../types";
 
 type Props = {
   statusOptions: {
     name: string;
     uid: string;
-  }[]
+  }[];
   headerColumns: {
     name: string;
     uid: string;
     sortable: boolean;
-  }[]
-  dispatch: Dispatch<CourseTableAction>
-  state: StateType
-
-}
-export const TableTopContent = ({ dispatch, headerColumns, state, statusOptions }: Props) => {
+  }[];
+  dispatch: Dispatch<CourseTableAction>;
+  state: StateType;
+};
+export const TableTopContent = ({
+  dispatch,
+  headerColumns,
+  state,
+  statusOptions,
+}: Props) => {
   const t = useTranslations("createOrEditCourseForm");
   const isTablet = useMedia("(min-width: 640px)", false);
+  const [columns, _] = useState(headerColumns);
+
   return (
     <div className="flex justify-between gap-3 items-center">
       <Input
         isClearable
         className="w-full sm:max-w-[44%]"
-        placeholder={t('searchByCourseName')}
+        placeholder={t("searchByCourseName")}
         startContent={<LuSearch className="text-default-300" />}
         value={state.filterValue}
         onClear={() => {
-          dispatch({ type: 'SET_FILTER_VALUE', payload: '' });
-          dispatch({ type: 'SET_PAGE', payload: 1 });
+          dispatch({ type: "SET_FILTER_VALUE", payload: "" });
+          dispatch({ type: "SET_PAGE", payload: 1 });
         }}
         onValueChange={(value) => {
           if (value) {
-            dispatch({ type: 'SET_FILTER_VALUE', payload: value });
-            dispatch({ type: 'SET_PAGE', payload: 1 });
+            dispatch({ type: "SET_FILTER_VALUE", payload: value });
+            dispatch({ type: "SET_PAGE", payload: 1 });
           } else {
-            dispatch({ type: 'SET_FILTER_VALUE', payload: '' });
+            dispatch({ type: "SET_FILTER_VALUE", payload: "" });
           }
         }}
       />
       <div className="flex gap-3">
         <Dropdown>
           <DropdownTrigger className="hidden md:flex">
-            <Button endContent={<LuChevronDown className="text-small" />} variant="flat">
-              {t('filterByStatus')}
+            <Button
+              endContent={<LuChevronDown className="text-small" />}
+              variant="flat"
+            >
+              {t("filterByStatus")}
             </Button>
           </DropdownTrigger>
           <DropdownMenu
@@ -60,9 +77,11 @@ export const TableTopContent = ({ dispatch, headerColumns, state, statusOptions 
             closeOnSelect={false}
             selectedKeys={state.statusFilter}
             selectionMode="multiple"
-            onSelectionChange={(keys) => dispatch({ type: 'SET_STATUS_FILTER', payload: keys as string })}
+            onSelectionChange={(keys) =>
+              dispatch({ type: "SET_STATUS_FILTER", payload: keys as string })
+            }
             classNames={{
-              list: "capitalize"
+              list: "capitalize",
             }}
           >
             {statusOptions.map((status) => (
@@ -75,7 +94,10 @@ export const TableTopContent = ({ dispatch, headerColumns, state, statusOptions 
 
         <Dropdown>
           <DropdownTrigger className="hidden md:flex">
-            <Button endContent={<LuChevronDown className="text-small" />} variant="flat">
+            <Button
+              endContent={<LuChevronDown className="text-small" />}
+              variant="flat"
+            >
               {t("hideSomeColumns")}
             </Button>
           </DropdownTrigger>
@@ -87,24 +109,37 @@ export const TableTopContent = ({ dispatch, headerColumns, state, statusOptions 
             closeOnSelect={false}
             selectedKeys={state.visibleColumns}
             selectionMode="multiple"
-            onSelectionChange={(keys) => dispatch({ type: 'SET_VISIBLE_COLUMNS', payload: keys as Set<string> })}
-
+            onSelectionChange={(keys) =>
+              dispatch({
+                type: "SET_VISIBLE_COLUMNS",
+                payload: keys as Set<string>,
+              })
+            }
             classNames={{
-              list: "capitalize"
+              list: "capitalize",
             }}
           >
-            {headerColumns.map((column) => (
-              <DropdownItem key={column.uid} className="capitalize" textValue={column.name}>
+            {columns.map((column) => (
+              <DropdownItem
+                key={column.uid}
+                className="capitalize"
+                textValue={column.name}
+              >
                 {column.name}
               </DropdownItem>
             ))}
           </DropdownMenu>
         </Dropdown>
-        <Button color="primary" href="/teacher/create" as={Link} startContent={<LuPlus />} isIconOnly={!isTablet}>
-          {isTablet ? t("newCourse"):""}
+        <Button
+          color="primary"
+          href="/teacher/create"
+          as={Link}
+          startContent={<LuPlus />}
+          isIconOnly={!isTablet}
+        >
+          {isTablet ? t("newCourse") : ""}
         </Button>
       </div>
     </div>
-
   );
-}
+};
