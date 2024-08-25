1ae4246d597e7815584589cffb25fdfeb4ff75f1
Bakate <b.ba@startupflow.io>
2024-08-25 08:48:11 +0200

    refactor(student-business): rename learn-business in student-business

---
+++ src/app/(businesses)/(student-business)/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import { Banner } from "@/components/banner";
+import { VideoPlayer } from "@/features/student/components/video-player";
+import { useGetChapterById } from "@/features/teacher/data/use-get-chapter-by-id";
+import { Divider } from "@nextui-org/react";
+import { useLocale, useTranslations } from "next-intl";
+
+type Props = {
+  params: {
+    courseId: string;
+    chapterId: string;
+  };
+};
+
+const ChapterPage = ({ params }: Props) => {
+  const { data: chapter, isLoading } = useGetChapterById(
+    params.chapterId,
+    params.courseId
+  );
+  const locale = useLocale();
+  const t = useTranslations("studentCourseById");
+
+  if (isLoading) return <div>{t("loading")}</div>;
+  if (!chapter) return <div>{t("noData")}</div>;
+
+  // get chapter with translations
+  const chapterTranslation = chapter.titlesAndDescriptions.find(
+    (translation) => translation.lang === locale
+  );
+
+  const isLocked = !chapter.isFree;
+
+  return (
+    <div>
+      {isLocked ? (
+        <Banner label={t("lockedChapterLabel")} variant="warning" />
+      ) : null}
+
+      <div className="grid gap-4 pb-24">
+        <div className="w-full">
+          <VideoPlayer
+            playbackId={chapter.playbackId!}
+            title={chapterTranslation?.title!}
+            isLocked={isLocked}
+            completeOnEnd={false}
+            courseId={params.courseId}
+            chapterId={params.chapterId}
+            nextChapterId={""}
+          />
+        </div>
+        <div className="p-4 flex flex-cols md:flex-row items-center justify-between">
+          <h2 className="text-2xl font-bold pb-2">
+            {chapterTranslation?.title}
+          </h2>
+          <p className="text-sm text-gray-500">
+            {chapterTranslation?.description}
+          </p>
+        </div>
+        <Divider />
+      </div>
+    </div>
+  );
+};
+
+export default ChapterPage;
