d1ed8b369c30eaf2e61c1235ae9a7bfff95dad0a
Bakate <b.ba@startupflow.io>
2024-08-30 12:21:42 +0200

    feat(student): optimize student courses layout with conditional rendering

--- src/app/(businesses)/(student-business)/courses/layout.tsx
+++ src/app/(businesses)/(student-business)/courses/layout.tsx
@@ -3,11 +3,13 @@ import NavbarComponent from "@/features/teacher/components/navbar";
 import { Sidebar } from "@/features/teacher/components/sidebar";
 import { useSession } from "next-auth/react";
 import { useTranslations } from "next-intl";
-import { redirect } from "next/navigation";
+import { redirect, usePathname } from "next/navigation";
 import { LuCompass, LuList } from "react-icons/lu";
 
 const StudentCoursesLayout = ({ children }: { children: React.ReactNode }) => {
   const session = useSession();
+  const pathname = usePathname();
+  const isLearning = pathname && pathname === "/courses";
 
   const t = useTranslations("Navigation");
   if (session?.status === "loading") {
@@ -31,27 +33,30 @@ const StudentCoursesLayout = ({ children }: { children: React.ReactNode }) => {
     },
   ];
 
-  return (
-    <div className="h-[100dvh]">
-      <div className=" fixed inset-y-0 w-full">
-        <NavbarComponent
-          routes={studentRoutes}
-          isTeacherPage={false}
-          isLearning={false}
-          isAuthenticated={isAuthenticatedStudent}
-        />
+  if (isLearning) {
+    return (
+      <div className="h-[100dvh]">
+        <div className=" fixed inset-y-0 w-full">
+          <NavbarComponent
+            routes={studentRoutes}
+            isTeacherPage={false}
+            isLearning={false}
+            isAuthenticated={isAuthenticatedStudent}
+          />
+        </div>
+        <div className="hidden md:grid w-56 fixed inset-y-0 z-50 bg-slate-50 dark:bg-slate-900">
+          <Sidebar
+            routes={studentRoutes}
+            isAuthenticated={isAuthenticatedStudent}
+          />
+        </div>
+        <main className="md:pl-60 pt-[80px] h-full max-w-6xl mx-auto">
+          {children}
+        </main>
       </div>
-      <div className="hidden md:grid w-56 fixed inset-y-0 z-50 bg-slate-50 dark:bg-slate-900">
-        <Sidebar
-          routes={studentRoutes}
-          isAuthenticated={isAuthenticatedStudent}
-        />
-      </div>
-      <main className="md:pl-60 pt-[80px] h-full max-w-6xl mx-auto">
-        {children}
-      </main>
-    </div>
-  );
+    );
+  }
+  return <>{children}</>;
 };
 
 export default StudentCoursesLayout;
