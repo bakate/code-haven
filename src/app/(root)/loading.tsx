8c927c926c938cdf3e0fb8dcd4d250ed8e170670
Bakate <b.ba@startupflow.io>
2024-09-03 08:58:28 +0200

    refactor: Replace loading spinner with Loader component in Loading module

--- src/app/(root)/loading.tsx
+++ src/app/(root)/loading.tsx
@@ -1,9 +1,7 @@
+import { Loader } from "@/components/loader";
+
 const Loading = () => {
-  return (
-    <div className="flex min-h-full items-center justify-center p-4 text-center">
-      <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-gray-900"></div>
-    </div>
-  );
+  return <Loader />;
 };
 
 export default Loading;
